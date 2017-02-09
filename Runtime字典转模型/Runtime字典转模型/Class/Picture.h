9973c96e464b2664e658e5becbb98b156d0d7cb2
Jeffrey <1441889663@qq.com>
2017-02-09 17:45:09 +0800

    demo
    字典转模型demo

---
+++ Runtime字典转模型/Runtime字典转模型/Class/Picture.h
@@ -0,0 +1,15 @@
+//
+//  Picture.h
+//  Runtime字典转模型
+//
+//  Created by JiangfanZhan on 2017/2/8.
+//  Copyright © 2017年 JiangfanZhan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Picture : NSObject
+
+@property (nonatomic, strong) NSURL *thumbnail_pic;
+
+@end
---
+++ Runtime字典转模型/Runtime字典转模型/Class/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Runtime字典转模型
+//
+//  Created by JiangfanZhan on 2017/2/8.
+//  Copyright © 2017年 JiangfanZhan. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
